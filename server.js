e590accf7f596561809c44c9ff5da8d00e878268
Minerororu <68819458+Minerororu@users.noreply.github.com>
2021-12-29 13:28:00 -0300

    testando email

--- server.js
+++ server.js
@@ -9,39 +9,39 @@ const documentos = [];
 
 let intervalTimer = '';
 app.post('/', function (req, res) {
-    if(req.body.documento){
-        if(documentos.filter(e => e.id === req.body.id).length == 0 && documentos.filter(e => JSON.parse(JSON.stringify(e)) === JSON.parse(JSON.stringify(req.body))).length == 0){
-          documentos.push(req.body);
-        } else {
-          let index = documentos.map(function(e) { return e.id}).indexOf(req.body.id);
-          documentos.splice(index, 1, req.body)
-        }
-        console.log(documentos)
-      } else if(req.body.assunto){
+  if(req.body.documento){
+    if(documentos.filter(e => e.id === req.body.id).length == 0 && documentos.filter(e => JSON.parse(JSON.stringify(e)) === JSON.parse(JSON.stringify(req.body))).length == 0){
+      documentos.push(req.body);
+    } else {
+        let index = documentos.map(function(e) { return e.id}).indexOf(req.body.id);
+        documentos.splice(index, 1, req.body)
+      }
+      console.log(documentos)
+    } else if(req.body.assunto){
         console.log(req.body)
         mandarEmail(req.body['assunto'], req.body['email'], req.body['corpo'], req.body['anexo']);
       }
 
     clearInterval(intervalTimer);
     intervalTimer = setInterval(() => {
-        documentos.map(documento => {
-            let dataValidade = new Date();
-            let strSplit = documento.dataValidade.split('/');
-            dataValidade.setDate(parseInt(strSplit[0]));
-            dataValidade.setMonth(parseInt(strSplit[1] - 1))
-            dataValidade.setFullYear(parseInt(strSplit[2]));
-            dataValidade.setDate(dataValidade.getDate() - documento.antecedencia);
-            console.log(dataValidade);
-            if(new Date() >= dataValidade){
-            console.log('chamei email linha 36');
-            mandarEmail("Documento próximo da Data de Validade", documento.email, "O documento: " + documento.documento + ", está próximo da data de validade, no dia: " + documento.dataValidade);
-            }
-        })
+      documentos.map(documento => {
+          let dataValidade = new Date();
+          let strSplit = documento.dataValidade.split('/');
+          dataValidade.setDate(parseInt(strSplit[0]));
+          dataValidade.setMonth(parseInt(strSplit[1] - 1))
+          dataValidade.setFullYear(parseInt(strSplit[2]));
+          dataValidade.setDate(dataValidade.getDate() - documento.antecedencia);
+          console.log(dataValidade);
+          if(new Date() >= dataValidade){
+          console.log('chamei email linha 36');
+          mandarEmail("Documento próximo da Data de Validade", documento.email, "O documento: " + documento.documento + ", está próximo da data de validade, no dia: " + documento.dataValidade);
+          }
+      })
     }, 24 * 60 * 60 * 1000);
     res.send('acharam o email jaaj')
 });
 
-function mandarEmail(assunto, destinatario, corpo, anexo = null){
+function mandarEmail(assunto, destinatario, corpo){
     console.log("Email enviado para " + destinatario)
     let transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -55,12 +55,6 @@ function mandarEmail(assunto, destinatario, corpo, anexo = null){
        to: destinatario,
        subject: assunto,
        text: corpo,
-       attachments: anexo ?  [
-        {
-            filename: "OrdemDeServico.pdf",
-            content: anexo?.output('arraybuffer')
-        }
-        ] : '',
      };
      transporter.sendMail(email, (error, info) => {
         if(error){
